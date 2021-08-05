162457f49b3ac1db360b59bbe25b779a1b8fd2e3
hugh-bowie <hughbowie@me.com>
2021-08-05 10:32:05 -0400

    Deploying

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(MONGODB_URI, {
-	useNewUrlParser: true,
 	useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 // routes
